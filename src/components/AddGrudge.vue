d81091286faa873c05893a9c836f830a1d424d3e
Mike Sausa <mikejsausa@gmail.com>
2017-01-10 11:24:28 -0700

    Added add grudge component

---
+++ src/components/AddGrudge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grudge-list">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      dateAdded: null,
+      offense: ''
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
